d79d862bfb17367f03785ac2a7d875b552a5eaab
charlie_laptop <hass@neuro.duke.edu>
2014-01-25 21:13:36 -0500

    minor mods

--- General/whoami.m
+++ General/whoami.m
@@ -4,11 +4,15 @@ function ID = whoami
 switch getmacaddr
     case '345'
         ID = 'glick_rig1';
-    case '234'
+        
+    case 'c4:2c:03:2a:29:3e'
         ID = 'hass_mbp';
+        
     case '123'
+        
     case '00-25-90-C1-B6-C2'
         ID = 'nuke';
+        
     otherwise
         ID = '';
 end
