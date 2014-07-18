f8058a176e52a162339de092e13c35e365504874
Rig 1 ephys <cahass@gmail.com>
2014-07-17 20:29:14 -0400

    updates to code that handles the Cl reversal potential analysis. I'm also updating ax.threshold so that it will accept arbitrairily large arrays This update will undoubtedly break other instances where threshold is used. I'll need to fix these too.

--- General/butterfilt.m
+++ General/butterfilt.m
@@ -2,11 +2,11 @@ function out = butterfilt(in, freqs, sampFreq, type)
 %
 % out = butterfilt(in, freqs, sampFreq, type)
 %
-%applies filtfilt using a butterworth filter kernel. Type should be 'high',
+%applies filter using a butterworth filter kernel. Type should be 'high',
 %'low', or 'stop'. If type = 'stop', freqs should contain two values.
 
 
 Wn = freqs./(sampFreq/2);
-order = 2;
+order = 4;
 [B,A] = butter(order, Wn, type);
 out = filter(B,A,in);
