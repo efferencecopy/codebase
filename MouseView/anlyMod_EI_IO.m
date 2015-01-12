2965a3e9732cacba76c1c463b5cfc2a3f79df0d6
charlie MBP <hass@neuro.duke.edu>
2015-01-12 12:13:56 -0500

    on going changes to the EI_IO analysis stream

--- MouseView/anlyMod_EI_IO.m
+++ MouseView/anlyMod_EI_IO.m
@@ -21,6 +21,16 @@ function params = anlyMod_EI_IO(params)
 
 
 
+% initialize the output arguments. this is important b/c I'll concatenate
+% into these arrays, and they must exist prior to the first concatenation.
+nGroups = size(params.isolatedCurrents, 1);
+for i_group = 1:nGroups;
+    group = params.isolatedCurrents{i_group,1};
+    params.avg.peak_nS.(group).cond = {[] []};
+    params.avg.peak_nS.(group).vals = {[] []};
+end
+
+
 % loop through each file, condition, and channel.
 nFiles = numel(params.files{2});
 for i_fid = 1:nFiles;
@@ -69,10 +79,13 @@ for i_fid = 1:nFiles;
             % pull out the max conductance for each pulse
             pOnIdx = params.tdict{i_fid}.pOnIdx{i_cond, i_ch};
             nPulses = numel(pOnIdx);
+            maxval_nS = nan(1, nPulses);
             for i_pulse = 1:nPulses
                 
                 % define an analysis window for each pulse
-                assert(t_anlyEndIdx < (pOnIdx(2)-pOnIdx(1)), 'ERROR: analysis window includes following pulse interval');
+                if nPulses>1
+                    assert(t_anlyEndIdx < (pOnIdx(2)-pOnIdx(1)), 'ERROR: analysis window includes following pulse interval');
+                end
                 anlyWindow = pOnIdx(i_pulse)+t_anlyStartIdx : pOnIdx(i_pulse)+t_anlyEndIdx;
                 snippet = params.avg.trace_pA{i_fid}{i_cond, i_ch}(anlyWindow);
                 
@@ -81,17 +94,96 @@ for i_fid = 1:nFiles;
                 [~, maxidx] = max(abs(snippet));
                 maxval_pA = snippet(maxidx);
                 
+                % calculate the bkgnd pA
+                bkgnd_pA = mean(params.avg.trace_pA{i_fid}{i_cond, i_ch}(anlyWindow(1)-21:anlyWindow(1)));
+                
+                % make sure the actual current takes into account the
+                % decaying current it may be riding on
+                maxval_pA = maxval_pA - bkgnd_pA;
+                
                 % convert to conductance
                 maxval_amps = maxval_pA ./ 1e12;
                 drivingForce_volts = drivingForce ./ 1e3;
                 maxval_siemans = maxval_amps ./ drivingForce_volts;
-                maxval_nS = maxval_siemans .* 1e9;
+                maxval_nS(i_pulse) = maxval_siemans .* 1e9;
                 
-                % store the maxval conductance
                 
             end
             
+            % store the maxval conductance once all the pulses have been
+            % accounted for
+            
+            % this line won't work for more complicated cases where there
+            % are mulitple frequencies. Consider a cell array for each
+            % i_cond. This would require more complicated unpacking later,
+            % but would allow the concatenation here.
+            params.avg.peak_nS.(fieldName).cond{i_ch} = cat(1, params.avg.peak_nS.(fieldName).cond{i_ch}, params.tdict{i_fid}.conds(i_cond,:));
+            params.avg.peak_nS.(fieldName).vals{i_ch} = cat(1, params.avg.peak_nS.(fieldName).vals{i_ch}, maxval_nS);
+            
+            
+        end % i_ch
+    end % i_cond
+end % i_fid
+
+
+
+% now spit out a summary figure. this will become very complicated
+% depending on which variables are interleaved, but for now, just assume
+% that the only one interleaved is the pulse amplitude. 
+for i_group = 1:nGroups
+    
+    group = params.isolatedCurrents{i_group,1};
+    
+    figure
+    set(gcf, 'name', group);
+    
+    
+    for i_ch = 1:2
+        subplot(2,1,i_ch)
+        if isempty(params.avg.peak_nS.(group).cond{i_ch})
+            continue
         end
+        x = params.avg.peak_nS.(group).cond{i_ch}(:,1);
+        [x, idx] = sort(x);
+        y = params.avg.peak_nS.(group).vals{i_ch};
+        y = y(idx);
+        plot(abs(x), abs(y), '-k.', 'linewidth', 2)
+        xlabel('pulse amplitude (V)')
+        ylabel('conductance (nS)')
     end
 end
+
+
+% plot the E/I ratio
+figure
+for i_ch = 1:2
+    subplot(2,1,i_ch)
+    if isempty(params.avg.peak_nS.excit.vals{i_ch})
+        continue
+    end
+    excit_nS = params.avg.peak_nS.excit.vals{i_ch};
+    excit_ledV = params.avg.peak_nS.excit.cond{i_ch}(:,1);
+    inhib_nS = params.avg.peak_nS.inhib.vals{i_ch};
+    inhib_ledV = params.avg.peak_nS.inhib.cond{i_ch}(:,1);
+    
+    % make sure that the nS values are sorted identically for the excit and
+    % inhib conditions
+    [excit_ledV, idx] = sort(excit_ledV, 'ascend');
+    inhib_ledV = inhib_ledV(idx);
+    assert(all(inhib_ledV == excit_ledV), 'ERROR: no correspondence b/w excit and inhib LED pow');
+    
+    ei_ratio = abs(excit_nS(idx))./abs(inhib_nS(idx));
+    plot(excit_ledV, ei_ratio, '-k.');
+    
+
+end
+
+
+
+
+
+
+
+
+
 
