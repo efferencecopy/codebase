5d1dea240614e9bec1044b657e25960241cf3703
charlie mint <cahass@gmail.com>
2015-02-11 11:16:50 -0500

    changes that hopefully will not conflict with stuff from my macbook

--- PopulationAnalysis/fiberVolleyAnalysis.m
+++ PopulationAnalysis/fiberVolleyAnalysis.m
@@ -261,7 +261,8 @@ for i_tf = 1:numel(structFields)
             tmp_trace = trace.(tType).(conds{i_cond});
             
             for i_ch = 1:size(tmp_trace,2)
-                tmp_trace(:,i_ch) = rmhum(tmp_trace(:,i_ch), sampFreq, winStart_idx, winEnd_idx, lines);
+                warning('RMHUM not in the loop')
+                %tmp_trace(:,i_ch) = rmhum(tmp_trace(:,i_ch), sampFreq, winStart_idx, winEnd_idx, lines);
             end
             
             trace.(tType).(conds{i_cond}) = tmp_trace;
