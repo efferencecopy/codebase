bd16c4dfa6df3d5f1d1b2794f3d2020dfe178763
charlie MBP <hass@neuro.duke.edu>
2015-08-07 17:18:16 -0400

    generalized the function for concatenatin further into a structure

--- General/structcat.m
+++ General/structcat.m
@@ -4,7 +4,23 @@ function out = structcat(s, field)
 % structures in a cell array of structures
 %
 % s should be a cell array of structures
-% field should be the field name
+% field should be the field name or cell array of nested names
 
+if iscell(field)
+    N = numel(field);
+else
+    N = 1;
+end
 
-out = cellfun(@(x,y) x.(y), s(:), repmat({field}, numel(s), 1), 'uniformoutput', false);
+switch N
+    case 1
+        out = cellfun(@(x,y) x.(y), s(:), repmat({field}, numel(s), 1), 'uniformoutput', false);
+    case 2
+        out = cellfun(@(x,y) x.(y{1}).(y{2}), s(:), repmat({field}, numel(s), 1), 'uniformoutput', false);
+    case 3
+        out = cellfun(@(x,y) x.(y{1}).(y{2}).(y{3}), s(:), repmat({field}, numel(s), 1), 'uniformoutput', false);
+    case 4
+        out = cellfun(@(x,y) x.(y{1}).(y{2}).(y{3}).(y{4}), s(:), repmat({field}, numel(s), 1), 'uniformoutput', false);
+    otherwise
+        error('indent level is too deep')
+end
